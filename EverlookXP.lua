cdf6d986c1ff3f283fcdaf584f17313dfd626dad
Dainel Vera <arubinofaux@gmail.com>
2021-09-10 09:11:01 -0600

    weekend 7x rate

--- EverlookXP.lua
+++ EverlookXP.lua
@@ -1,7 +1,7 @@
 -- Configuration settings
 -- Default values
 XP_Rate = "5";
-XP_Command = ".mod xprate "..XP_Rate;
+XP_Weekend_Rate = "7";
 Accepted_Realm = "Dunemaul"
 Player_Language = GetDefaultLanguage("PLAYER");
 General_Channel = 1;
@@ -16,10 +16,11 @@ local frame = CreateFrame("Frame");
 frame:RegisterEvent("CHAT_MSG_CHANNEL_NOTICE")
 frame:SetScript("OnEvent", function(frame, event)
   if ( XP_Set == false and Current_Realm == Accepted_Realm ) then
-	  XP_Set = true;
+		XP_Set = true;
 
     ChatFrame1:AddMessage("EverlookXP: Setting XP Rate to "..XP_Rate.."x on realm "..Current_Realm);
-
-    SendChatMessage(XP_Command, "CHANNEL", Player_Language, General_Channel);
+			
+		SendChatMessage(".mod xprate "..XP_Rate, "CHANNEL", Player_Language, General_Channel);
+		SendChatMessage(".mod xprate "..XP_Weekend_Rate, "CHANNEL", Player_Language, General_Channel);
 	end
 end)
